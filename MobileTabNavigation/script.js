5761e38a048aaa1064d5ecdd82dd52376d319a63
Sugam Chhetri <developer.sugamchhetri@gmail.com>
2021-09-05 11:20:53 +0530

    Adding Mobile Tab Navigation Project

---
+++ MobileTabNavigation/script.js
@@ -0,0 +1,19 @@
+const lists = document.querySelectorAll("nav ul li");
+const images = document.querySelectorAll(".content");
+
+
+// My Logic
+lists.forEach((list, idx) => {
+  list.addEventListener("click", () => {
+    removeActiveClasses();
+    list.classList.add("active");
+    images[idx].classList.add("show");
+  });
+});
+
+function removeActiveClasses() {
+  for (let i = 0; i < images.length; i++) {
+    lists[i].classList.remove("active");
+    images[i].classList.remove("show");
+  }
+}
